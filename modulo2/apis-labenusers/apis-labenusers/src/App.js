687f44d49d96c176ebc811a24f675362f5bd0739
fernandafmedina <fernandaf.medina@gmail.com>
2022-01-25 18:53:28 -0300

    Começo exercicio apis

---
+++ modulo2/apis-labenusers/apis-labenusers/src/App.js
@@ -0,0 +1,17 @@
+import logo from './logo.svg';
+import BotaoDeTrocar from './BotaoTrocar';
+
+
+
+function App() {
+  return (
+    <div className="App">
+    <BotaoDeTrocar >Trocar De Página</BotaoDeTrocar>
+    <input></input>
+    <input></input>
+    <button> Criar Usuario </button> 
+   </div>
+  );
+}
+
+export default App;
---
+++ modulo2/apis-labenusers/apis-labenusers/src/BotaoTrocar.js
@@ -0,0 +1,14 @@
+import React from "react";
+
+import styled from "styled-components";
+
+const BotaoDeTrocar = styled.button`
+   display: flex;
+   height: 30px;
+   justify-content: flex-end;
+   background: #0099FF;
+   border: 2px solid;
+
+
+`;
+export default BotaoDeTrocar;
